3775c841a96f40549e1d121733cebb05a84598cd
alejandro <alemures@hotmail.com>
2015-10-13 10:57:53 +0100

    Initial commit

---
+++ test/test.js
@@ -0,0 +1,11 @@
+'use strict';
+
+var describe = require('mocha').describe;
+var it = require('mocha').it;
+var chai = require('chai');
+
+describe('test', function() {
+  it('should be true', function() {
+    chai.assert(true, 'true');
+  });
+});
